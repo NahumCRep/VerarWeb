2b5f8a1cc65202fdf95f7378705f591c8eb78495
Nahum Casco <nahumcasco.v@gmail.com>
2021-11-08 19:40:33 -0500

    Productos Page Layout

--- src/assets/pages/Products.js
+++ src/assets/pages/Products.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import '../css/products-styles/products.css';
 
 const Prodcuts = () => {
     return(
-        <div>
-            Products Page
+        <div className="products-body">
+            <div className="second-nav"></div>
+            <div className="product-container">
+                <div className="product-side-menu">
+                    <ul>
+                        <li>Parches</li>
+                        <li>Ropa</li>
+                        <li>Mascarilla</li>
+                        <li>Oficina</li>
+                    </ul>
+                </div>
+                <div className="product-content"></div>
+                <div className="product-list"></div>
+            </div>
         </div>
     )
 }
