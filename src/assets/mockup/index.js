984afb66bd89fa001a3cd20019732ee846a87cb5
Nahum Casco <nahumcasco.v@gmail.com>
2022-05-13 21:50:43 -0500

    products mockup

---
+++ src/assets/mockup/index.js
@@ -0,0 +1,65 @@
+import gorra from '../images/productos/gorras/gorra-b-1.jpg'
+import camisah from '../images/productos/camisetas/camiseta-b-h.jpg'
+import camisas from '../images/productos/camisetas/camiseta-va.jpg'
+import mascarillaV from '../images/productos/mascarrillas/mak1.jpg'
+import mascarillaA from '../images/productos/mascarrillas/mak2.jpg'
+import mascarillaB from '../images/productos/mascarrillas/mak3.jpg'
+import carnet from '../images/productos/oficina/carnet.jpg'
+import carpeta from '../images/productos/oficina/carpeta.jpg'
+import colgante from '../images/productos/oficina/colgante.jpg'
+import kit from '../images/productos/oficina/kit-papeleria.jpg'
+import sobre from '../images/productos/oficina/sobre.jpg'
+import tarjeta from '../images/productos/oficina/tarjeta.jpg'
+
+const mockup = [
+    {
+        name:'gorra',
+        img: gorra
+    },
+    {
+        name:'mascarilla verde',
+        img: mascarillaV
+    },
+    {
+        name:'mascarilla azul',
+        img: mascarillaA
+    },
+    {
+        name:'mascarilla blanca',
+        img: mascarillaB
+    },
+    {
+        name:'camiseta blanca',
+        img: camisah
+    },
+    {
+        name:'camiseta verde y azul',
+        img: camisas
+    },
+    {
+        name:'carnet',
+        img: carnet
+    },
+    {
+        name:'carpeta',
+        img: carpeta
+    },
+    {
+        name:'colgante',
+        img: colgante
+    },
+    {
+        name:'kit de oficina',
+        img: kit
+    },
+    {
+        name:'sobre',
+        img: sobre
+    },
+    {
+        name:'tarjeta',
+        img: tarjeta
+    }
+]
+
+export default mockup
