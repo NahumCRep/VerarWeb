bb6711af68adb4da5512161d1caf51586c8116d8
Nahum Casco <nahumcasco.v@gmail.com>
2022-05-15 18:44:54 -0500

    last info block added

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,13 +3,13 @@ import '../css/products-styles/product.css'
 import packageImg from '../assets/images/inf-p-4.png'
 import packageBack from '../assets/images/inf-p-1.jpg'
 import PromotionItems from '../components/products-components/PromotionItems'
-import {AiFillGift} from 'react-icons/ai'
+import { AiFillGift } from 'react-icons/ai'
 
 
 const Product = () => {
   return (
     <section className='product_page__section'>
-      <div className='product_page__container'>
+      <article className='product_page__container'>
         <img src={packageImg} alt="package" className='product_page_main_img' />
         <div className='product_page__info'>
           <h1>Parches Verar</h1>
@@ -17,8 +17,8 @@ const Product = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt posuere quam, convallis efficitur mi viverra et. Duis imperdiet urna vel tincidunt luctus. Fusce facilisis diam ut leo gravida, eget scelerisque urna convallis. Phasellus hendrerit tincidunt elementum. Quisque faucibus, odio quis gravida iaculis, nisi felis porta nibh, nec varius nulla ex non neque. Ut vulputate nunc eget justo dignissim ornare. Nulla congue nec odio sit amet viverra. Quisque fringilla pharetra magna, id consectetur augue auctor non. Nulla ac orci non nibh commodo tincidunt. Nunc hendrerit dui eu fringilla auctor.
           </p>
         </div>
-      </div>
-      <div className='product_page__container'>
+      </article>
+      <article className='product_page__container'>
         <div className='product_page__info'>
           <h1>Caraterísticas</h1>
           <ul>
@@ -34,6 +34,35 @@ const Product = () => {
           </ul>
         </div>
         <img src={packageBack} alt='package' className='product_page_img_circular' />
+      </article>
+      <article className='product_page_info_block'>
+        <h1>¿Porqué Utilizar Parches Verar?</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ante mi, malesuada id commodo sit amet, dapibus feugiat mauris.
+          Curabitur ultricies feugiat fringilla. Nulla posuere vulputate eros sit amet egestas. Donec interdum, augue a facilisis auctor, lorem
+          elit fermentum augue, nec feugiat nisi odio quis ante. Vestibulum at laoreet purus. Sed non varius nisi, at dictum sapien. Quisque
+          faucibus ac est nec interdum. Integer rhoncus vitae dolor id congue. Cras eleifend varius auctor. Aliquam at leo nec leo vehicula commodo.
+          Aliquam quis libero ut enim varius bibendum. Sed ac aliquam orci.
+          <br /> <br />
+          Cras nec tristique urna. Maecenas nulla lacus, volutpat eget lectus eu, cursus sodales felis. Vestibulum blandit ipsum eget lorem elementum dictum.
+          Vivamus eget pellentesque nibh, sit amet interdum diam. Phasellus posuere dapibus ex, id mollis tellus lobortis nec. Vivamus nec eleifend erat.
+          Vivamus quis felis euismod velit venenatis vulputate. Donec ut mauris at ipsum imperdiet finibus sed pulvinar lacus. Mauris placerat molestie semper.
+        </p>
+      </article>
+
+      <div className='product_page__info'>
+        <h1>Beneficios</h1>
+        <ul>
+          <li>Nunc eu ex elementum, volutpat mi ut, iaculis velit.</li>
+          <li>Quisque ut felis ultrices, hendrerit magna tempus, bibendum nisl.</li>
+          <li>Nulla ac justo vitae leo mattis mollis.</li>
+          <li>In eget erat a justo fermentum lobortis a vitae velit.</li>
+          <li>Nunc ut leo id turpis pharetra mattis a nec diam.</li>
+          <li>Sed ultricies ante sit amet nibh ullamcorper lacinia.</li>
+          <li>Quisque euismod ante vitae risus rutrum, id tempor orci sagittis.</li>
+          <li>Sed efficitur ipsum a feugiat volutpat.</li>
+          <li>Suspendisse eu augue blandit lacus maximus sagittis ut in lectus.</li>
+        </ul>
       </div>
 
       <div className='product_page_promotion'>
